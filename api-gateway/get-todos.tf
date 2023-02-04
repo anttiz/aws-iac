786473ee3405bae90c023a65ff073c47a3335285
Antti Lehtonen <antti.lehtonen@wunderdog.fi>
2023-02-04 18:21:36 +0200

    CORS works for get-todos.

--- api-gateway/get-todos.tf
+++ api-gateway/get-todos.tf
@@ -42,3 +42,54 @@ resource "aws_lambda_permission" "apigw_get_todos_lambda_permission" {
 
   source_arn = "arn:aws:execute-api:${var.aws_region}:${var.account_id}:${aws_api_gateway_rest_api.rest_api.id}/*/${aws_api_gateway_method.get_todos_api_method.http_method}${aws_api_gateway_resource.get_todos_resource.path}"
 }
+
+# cors
+# OPTIONS HTTP method.
+resource "aws_api_gateway_method" "options" {
+  rest_api_id      = aws_api_gateway_rest_api.rest_api.id
+  resource_id      = aws_api_gateway_resource.get_todos_resource.id
+  http_method      = "OPTIONS"
+  authorization    = "NONE"
+  api_key_required = false
+}
+
+# OPTIONS method response.
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.get_todos_resource.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+# OPTIONS integration.
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id          = aws_api_gateway_rest_api.rest_api.id
+  resource_id          = aws_api_gateway_resource.get_todos_resource.id
+  http_method          = "OPTIONS"
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+  request_templates = {
+    "application/json" : "{\"statusCode\": 200}"
+  }
+}
+
+# OPTIONS integration response.
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.get_todos_resource.id
+  http_method = aws_api_gateway_integration.options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
